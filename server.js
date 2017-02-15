584f0c8005bd0fe9e30228d83fd54e67d5ba6c77
arusmanukyan <manukyan.arus@gmail.com>
2017-02-14 19:07:11 -0800

    server

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://localhost/mongooseHomework");
 var db = mongoose.connection;
 
 // Show any mongoose errors
